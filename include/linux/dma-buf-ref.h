2d4a4fd0dd3cb4052c2d4224e9d485532b3f74ac
Levi Marvin <levimarvin@icloud.com>
2023-07-23 17:27:43 +0800

    include: Fix the redefined funtions error at link time.
      Functions:
       * msm_dma_buf_create(...)
       * msm_dma_buf_destory(...)
      In the file:
       * include/linux/dma-buf-ref.h
    
    Signed-off-by: Levi Marvin <levimarvin@icloud.com>

--- include/linux/dma-buf-ref.h
+++ include/linux/dma-buf-ref.h
@@ -25,8 +25,8 @@ static inline int dma_buf_ref_show(struct seq_file *s,
 {
 	return -ENOMEM;
 }
-struct msm_dma_buf *msm_dma_buf_create(struct dma_buf *dma_buf) { return ERR_PTR(-ENOMEM); }
-void msm_dma_buf_destroy(struct dma_buf *dma_buf) {}
+static inline struct msm_dma_buf *msm_dma_buf_create(struct dma_buf *dma_buf) { return ERR_PTR(-ENOMEM); }
+static inline void msm_dma_buf_destroy(struct dma_buf *dma_buf) {}
 #endif
 
 
